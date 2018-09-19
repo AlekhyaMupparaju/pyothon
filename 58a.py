4eee80f898394bac2061013179b3d43338fda5f4
AlekhyaMupparaju <42571970+AlekhyaMupparaju@users.noreply.github.com>
2018-09-19 10:44:07 +0530

    Update 58a.py

--- 58a.py
+++ 58a.py
@@ -1,6 +1,6 @@
-r,s=map(int,raw_input().split())
+g,h=map(int,raw_input().split())
 list=[int(x) for x in raw_input().split()]
-if r and s in list:
+if g and h in list:
     print "yes"
 else:
     print "No"
