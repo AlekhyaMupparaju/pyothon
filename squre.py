f157501528ef3bf651ebf347bd8e85166d13b2ca
AlekhyaMupparaju <42571970+AlekhyaMupparaju@users.noreply.github.com>
2019-02-21 04:00:47 -0300

    Create squre.py

---
+++ squre.py
@@ -0,0 +1,8 @@
+s1,a1=map(int,(raw_input()).split())
+s2,a2=map(int,(raw_input()).split())
+s3,a3=map(int,(raw_input()).split())
+s4,a4=map(int,(raw_input()).split())
+if(s1+s2+s3+s4==a1+a2+a3+a4):
+    print("yes")
+else:
+    print("no")
